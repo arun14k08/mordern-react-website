6c6fcb22af8434b31c90e5b6a4cd73019c2b3c99
ARUN KUMAR D <arun14k08@gmail.com>
2024-02-12 23:31:21 +0530

    added all the dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
-function App() {}
+import React from "react";
+import styles from "./style.js";
+
+const App = () => {
+    return (
+        <div>
+            <div className={`${styles.padding} ${styles.flexCenter}`}>
+                <div className={`${styles.boxWidth}`}>Navbar</div>
+            </div>
+
+            <div className={` ${styles.flexStart}`}>
+                <div className={`${styles.boxWidth}`}>Hero</div>
+            </div>
+        </div>
+    );
+};
 
 export default App;
