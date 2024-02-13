61e22a8b750adbca59ece600bd81ee22838c0251
ARUN KUMAR D <arun14k08@gmail.com>
2024-02-13 22:47:07 +0530

    Components added and navbar completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import styles from "./style.js";
+
+import {
+    Navbar,
+    Hero,
+    Stats,
+    Business,
+    Billing,
+    CardDeal,
+    Testimonials,
+    Clients,
+    CTA,
+    Footer,
+} from "./components";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+    return (
+        <div>
+            <div className={`${styles.paddingX} ${styles.flexCenter} bg-primary`}>
+                <div className={`${styles.boxWidth}`}>
+                    <Navbar />
+                </div>
+            </div>
 
-export default App
+            <div className={`${styles.flexStart} bg-primary`}>
+                <div className={`${styles.boxWidth}`}>
+                    <Hero />
+                </div>
+            </div>
+
+            <div
+                className={`${styles.paddingX} ${styles.flexStart} bg-primary`}
+            >
+                <Stats />
+                <Business />
+                <Billing />
+                <CardDeal />
+                <Testimonials />
+                <Clients />
+                <CTA />
+                <Footer />
+                <div className={`${styles.boxWidth}`}></div>
+            </div>
+        </div>
+    );
+};
+
+export default App;
