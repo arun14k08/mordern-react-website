43c899dd7014f50d64162f8e870aaf39a7521551
ARUN KUMAR D <arun14k08@gmail.com>
2024-02-16 23:36:33 +0530

    imorted assets for business section

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { features } from "../constants/index.js";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
 const Business = () => {
-  return (
-    <div>Business</div>
-  )
-}
+    return (
+        <section id="features" className={layout.section}>
+            <div className={layout.sectionInfo}>
+                <h2>
+                    You do the business, <br className="sm:block hidden" />
+                    we'll handle the money.
+                </h2>
+            </div>
+        </section>
+    );
+};
 
-export default Business
+export default Business;
